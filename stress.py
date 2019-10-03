b7607f5c30ad17bda495ad4d901d8e5a006f86f2
Irfan <irfan@Irfans-MacBook-Pro.local>
2019-10-03 12:59:54 +0530

    change ue

--- stress.py
+++ stress.py
@@ -5,8 +5,10 @@ import concurrent.futures
 import requests
 
 async def main():
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    workers = int(input("Enter the max number of workers: "))
+    get_requests = int(input("Enter the number of GET requests you want to send: "))
+    print("Sending GET...")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
 
         loop = asyncio.get_event_loop()
         futures = [
@@ -16,7 +18,7 @@ async def main():
                 'https://indiaforchange.org/'
             )
             #Change number in range according to the requests you want to send
-            for i in range(20)
+            for i in range(get_requests)
         ]
         for response in await asyncio.gather(*futures):
             pass
